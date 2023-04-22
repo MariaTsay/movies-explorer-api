22387d7cb49cd013b25a67b0028fd3721d67d4b0
Maria Tsay <mashuny2@yandex.ru>
2023-04-22 13:08:35 +0300

    backend

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -21,6 +23,16 @@ mongoose
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// подключаем rate-limiter
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
 
 app.use(requestLogger); // подключаем логгер запросов
